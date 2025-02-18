cc4cdb1514785e4367bd1dd34cc262fa34428536
Erick David Cohen <erickdcohen@gmail.com>
2025-02-18 16:37:53 -0500

    Finishes first basic shapes and fixes background color issue

--- src/routes/basic-shapes/+page.svelte
+++ src/routes/basic-shapes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { Tween } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
 	type Circle = {
 		x: number;
 		y: number;
@@ -29,6 +32,29 @@
 		{ x: 10, y: 210, width: 5, height: 40 }
 	]);
 
+	let rectanglesText: Rectangle[] = $state([
+		{ x: 10, y: 10, width: 5, height: 40 },
+		{ x: 10, y: 60, width: 5, height: 40 },
+		{ x: 10, y: 110, width: 5, height: 40 },
+		{ x: 10, y: 160, width: 5, height: 40 },
+		{ x: 10, y: 210, width: 5, height: 40 }
+	]);
+
+	const widths = Tween.of(() => rectanglesText.map((rec) => rec.width), {
+		duration: 1000,
+		easing: cubicOut
+	});
+
+	const handleClick = () => {
+		rectanglesText = [
+			{ x: 10, y: 10, width: 450 * Math.random(), height: 40 },
+			{ x: 10, y: 60, width: 320 * Math.random(), height: 40 },
+			{ x: 10, y: 110, width: 160 * Math.random(), height: 40 },
+			{ x: 10, y: 160, width: 80 * Math.random(), height: 40 },
+			{ x: 10, y: 210, width: 10 * Math.random(), height: 40 }
+		];
+	};
+
 	// Circles transitions
 	setTimeout(() => {
 		circles = [
@@ -54,6 +80,7 @@
 
 <h1 class="mb-4 text-center text-2xl text-lime-300">01 -- Basic Shapes</h1>
 
+<!-- Circles -->
 <div id="circles" class="text-center text-xl text-slate-100">
 	<h2 class="text-2xl">Circles</h2>
 	<div>
@@ -65,14 +92,47 @@
 	</div>
 </div>
 
+<!-- Rectangles -->
 <div id="rectangles" class="text-center text-xl text-slate-100">
 	<h2 class="text-2xl">Rectangles</h2>
 	<div>
 		<svg viewBox="0 0 600 300">
 			{#each rectangles as rec}
-				<rect x={rec.x} y={rec.y} width={rec.width} height={rec.height} fill="white" />
+				<rect
+					id="reg-rect"
+					x={rec.x}
+					y={rec.y}
+					width={rec.width}
+					height={rec.height}
+					fill="white"
+				/>
 			{/each}
 		</svg>
+	</div>
+</div>
+
+<!-- Rectangles with Text -->
+<div id="rectanglesText" class="text-center text-xl text-slate-100">
+	<h2 class="text-2xl">Rectangles with Text</h2>
+	<div>
+		<svg viewBox="0 0 600 300">
+			{#each rectangles as rec, i}
+				<rect x={rec.x} y={rec.y} width={widths.current[i]} height={rec.height} fill="white" />
+				<text
+					fill="white"
+					x={rec.x + widths.current[i] + 10}
+					y={rec.y + rec.height / 2 + 5}
+					text-anchor="start"
+				>
+					{widths.current[i].toFixed(0)}</text
+				>
+			{/each}
+		</svg>
+		<button
+			onclick={handleClick}
+			class="m-2 mb-10 rounded-lg bg-yellow-400 px-2 py-2 text-lg font-semibold text-gray-900 hover:bg-yellow-500"
+			>Animate</button
+		>
 	</div>
 </div>
 
@@ -81,7 +141,7 @@
 		transition: all 1s ease;
 	}
 
-	rect {
+	#reg-rect {
 		transition: all 1s ease;
 	}
 </style>
